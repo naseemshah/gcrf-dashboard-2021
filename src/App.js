ddf03c405860f9783de70b3b2b65a0ebdc40cfed
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-04-11 17:35:06 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse'
+import { useEffect, useState } from 'react';
+
+import Loading from './components/common/Loading'
 
 function App() {
+  let [isLoading,setIsLoading] = useState(true)
+  useEffect(()=>{
+    setIsLoading(true)
+    Papa.parse("/data.csv", {
+      download: true,
+      complete: function(results) {
+        setIsLoading(false)
+        console.log(results);
+      }
+    });
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        isLoading && <Loading />
+      }
     </div>
   );
 }
